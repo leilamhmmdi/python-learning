32ed073ea399ea140b7fdfbbc5eb6be79a4647ef
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-18 15:32:03 +0330

    05_strings.py
    Add string examples

---
+++ 05_strings.py
@@ -0,0 +1,56 @@
+"""
+String Examples
+"""
+
+# String concatenation
+
+x = "123"
+print(x + "2345")
+
+# String repetition
+
+text = "hello"
+print(text * 3)
+
+# Convert string to integer
+
+number = "123"
+print(int(number) + 4)
+
+# Floating-point precision
+
+x = 0.3
+y = 0.6
+
+print(x + y)
+
+# Single and double quotes
+
+text = "I'm Leila"
+print(text)
+
+text = 'I\'m Leila'
+print(text)
+
+# Raw string
+
+path = r"c:\download\new\telegram"
+print(path)
+
+# Multi-line string
+
+message = """hello
+world
+how
+are
+you
+?"""
+
+print(message)
+
+# String methods
+
+text = "hello world"
+
+print(text.capitalize())
+print(text.isdigit())
