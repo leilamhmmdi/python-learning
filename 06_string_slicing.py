e79d2773b85f78cdbe17b01cd70f4581e1bfe9f5
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-18 15:33:42 +0330

    06_string_slicing.py
    Add string slicing examples

---
+++ 06_string_slicing.py
@@ -0,0 +1,23 @@
+"""
+String Slicing Examples
+"""
+
+text = "hello world"
+
+# Characters from index 3 to 7
+
+print(text[3:8])
+
+# Entire string
+
+print(text[:])
+
+# Every second character
+
+print(text[::2])
+
+# Reverse a string
+
+number = "1234567"
+
+print(number[::-1])
