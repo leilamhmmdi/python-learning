96839d5daa771481a1ad46d4c6946312eaf682f6
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-18 15:29:44 +0330

    04_numbers.py

---
+++ 04_numbers.py
@@ -0,0 +1,22 @@
+"""
+Numbers Examples
+"""
+
+# Complex numbers
+
+x = 3 + 4j
+
+print(x + 5)
+print(type(x))
+
+# Integer method
+
+x = 3
+print(x.__add__(5))
+
+# Built-in functions
+
+print(abs(-3))
+print(pow(3, 4))
+print(round(39.35636748, 2))
+print(float(39))
