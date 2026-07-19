9d2812a01b3499e4c62e35a87277ec362024b574
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-19 12:38:56 +0330

    Create 10_dictionaries.py

---
+++ 10_dictionaries.py
@@ -0,0 +1,21 @@
+# Creating a Dictionary
+d = {
+    "varzesh": 10.25,
+    "riazi": 6.5,
+    "andishe1": 4,
+    "andishe2": 9.75
+}
+print(d)
+
+# Accessing Values
+print(d["varzesh"])
+print(d["riazi"])
+
+# Dictionary Keys
+print(d.keys())
+
+# Dictionary Values
+print(d.values())
+
+# Dictionary Items
+print(d.items())
