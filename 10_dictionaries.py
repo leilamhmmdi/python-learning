9f9f7aab7ec94d44516703d93b5644f03a1ed48e
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-20 16:09:35 +0330

    10_dictionaries.py

--- 10_dictionaries.py
+++ 10_dictionaries.py
@@ -14,8 +14,34 @@ print(d["riazi"])
 # Dictionary Keys
 print(d.keys())
 
+d = {'a': 1, 'b': 2, 'c': 3, 'a': 4}
+print(d)
+
 # Dictionary Values
 print(d.values())
 
 # Dictionary Items
 print(d.items())
+
+
+# Add a new key
+d = {'a': 1, 'b': 2, 'c': 3}
+d['d'] = 56
+print(d)
+
+# Empty dictionary
+d = {}
+d['d'] = 56
+print(d)
+
+# Get key and value from user
+key1 = input('Enter first key: ')
+value1 = input('Enter first value: ')
+key2 = input('Enter second key: ')
+value2 = input('Enter second value: ')
+
+d = {}
+d[key1] = value1
+d[key2] = value2
+
+print(d)
