2f928502641b0482c1421c0391bc4e58d6d27a68
leilamhmmdi <leilamhmmdi04@gmail.com>
2026-07-20 16:18:25 +0330

    12_booleans.py

---
+++ 12_booleans.py
@@ -0,0 +1,30 @@
+# Booleans
+t = True
+print(t + 1)
+
+print(bool(''))
+print(bool([]))
+print(bool({}))
+print(bool(0))
+print(bool(None))
+
+x = 5
+y = 10
+
+print(x < y and x == 6)
+print(x < y or x == 6)
+
+x = 0
+y = 10
+print(x and y)
+
+x = 5
+y = 10
+print(x and y)
+
+print(x or y)
+
+# NoneType
+n = None
+print(n)
+print(type(n))
